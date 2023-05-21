1837c85f98195f3235e4c14db2f9dd5213377c5a
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-05-21 15:39:12 +0200

    add login endpoint

--- backend/core/routes/user.py
+++ backend/core/routes/user.py
@@ -1,14 +1,16 @@
 
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, Depends
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from starlette import status
-from core.models.user import User, UpdateUser
+from core.models.user import User
 from core.database import db
+from core.security import is_match, Jwt, Claims
 
 router = APIRouter(prefix='/user')
 
 @router.post('/register')
-async def new_user(user: User = Body(...)):
+async def register(user: User = Body(...)):
     user = user.to_dict()
 
     res = check_unique(user)
@@ -19,6 +21,24 @@ async def new_user(user: User = Body(...)):
 
     content = {'data': {'id': res.inserted_id}}
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=content)
+
+
+class LoginBody(BaseModel):
+    username: str
+    password: str
+
+@router.post('/login')
+async def login(form_data: LoginBody = Depends()):
+    un = form_data.username
+    user = db.user.find_one({'username': un})
+    if user is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'error':f'no user with such username {un}'})
+    if not is_match(form_data.password, user['password']):
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'error': 'password do not match'})
+
+    token = Jwt.encode(Claims(user['_id']))
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={'data': {'token': token}})
+
 
 def check_unique(user: dict) -> bool:
     to_check = ['username', 'email', 'phone_number']
